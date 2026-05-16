456466a13a61691be9a2102e9f80cde0a6636023
Kiren Paul <kirenpaul@gmail.com>
2026-05-16 14:10:33 +0700

    feat(pwa): update manifest for improved icon purposes and add skipWaiting message handler in service worker

--- service-worker.js
+++ service-worker.js
@@ -38,8 +38,13 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
   );
+});
+
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
 });
 
 self.addEventListener('activate', event => {
